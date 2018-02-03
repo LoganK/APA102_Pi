0791181e34ac7a9817ed41cb90a412add7719529
Logan Kennelly <logank@sent.com>
2018-02-03 07:44:16 +0000

    Make examples_larson a full-fledge program

--- examples_larson.py
+++ examples_larson.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Classic Larson scanner in a tasteful red."""
 from apa102 import APA102, Pixel
+import argparse
 from math import ceil
 import time
 
@@ -29,17 +30,30 @@ def create_larson(strip, start, end):
             bright -= b_step
     return next
 
-with APA102(num_led=80, mosi = None, sclk = 11) as strip:
-    larson1 = create_larson(strip, 0, strip.num_led // 2 - 1)
-    larson2 = create_larson(strip, strip.num_led // 2, strip.num_led  - 1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Display a larson scanner.')
+    parser.add_argument('num_led', type=int, default=80, nargs='?',
+                        help='The number of LEDs in the strip')
+    parser.add_argument('mosi', type=int, default=10, nargs='?',
+                        help='The pin for SPI MOSI. 10 corresponds to the Raspberry Pi hardware SPI.'
+                        ' Set negative for console debug.')
+    parser.add_argument('sclk', type=int, default=11, nargs='?',
+                        help='The pin for SPI SCLK. 11 corresponds to the Raspberry Pi hardware SPI.')
+    parser.add_argument('--duration', dest='duration_s', type=float, default=7,
+                        help='How long the scanner should run in seconds.')
+    args = parser.parse_args()
 
-    # Run for 10 seconds
-    end_time = time.time() + 7
-    while time.time() < end_time:
-        strip.blank()
-        larson1()
-        larson2()
-        strip.show()
-        time.sleep(0.008)
+    with APA102(args.num_led, mosi=args.mosi, sclk=args.sclk) as strip:
+        larson1 = create_larson(strip, 0, strip.num_led // 2 - 1)
+        larson2 = create_larson(strip, strip.num_led // 2, strip.num_led  - 1)
+
+        # Run for 10 seconds
+        end_time = time.time() + args.duration_s
+        while time.time() < end_time:
+            strip.blank()
+            larson1()
+            larson2()
+            strip.show()
+            time.sleep(0.008)
 
 
